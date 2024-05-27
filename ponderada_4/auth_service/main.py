27005f0ed4cdf41363a98628c1f5f9f929c71ddf
cmtabr <caio.abreu@sou.inteli.edu.br>
2024-05-27 01:00:33 -0300

    feat(loggers): :loud_sound: Adds loggers for auth and image processor files

--- ponderada_4/auth_service/main.py
+++ ponderada_4/auth_service/main.py
@@ -1,8 +1,15 @@
+import logging
+
 from fastapi import FastAPI, HTTPException
 from sqlalchemy import select, insert
+
+
 from schemas.user import UserRequest
 from models.user_table import conn, UserTable
 from helpers.jwt_handler import signJWT
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -14,19 +21,25 @@ async def login(user_request: UserRequest):
                                     )
         result = conn.execute(query)
         if result:
+            logger.info("User created successfully for email: %s", user_request.email)
             return {"Response": "Usuário cadastrado"}
-    except Exception as e: 
+    except Exception as e:
+        logger.error("Error during signup for email: %s, Error: %s", user_request.email, str(e))
         return HTTPException(status_code=400, detail=str(e))
 
 @app.post("/user/login", tags=["User"], status_code=200)
 async def login_user(user_request: UserRequest):
+    logger.info("Login request received for email: %s", user_request.email)
     try:
         query = select(UserTable).where(UserTable.email == user_request.email)
         result = conn.execute(query).fetchone()
         if result:
+            logger.info("User logged in successfully for email: %s", user_request.email)
             token = signJWT(result.id)
             return {"message": "User logged in successfully", "token": token}
         else:
+            logger.warning("Invalid login credentials for email: %s", user_request.email)
             raise HTTPException(status_code=400, detail="Invalid credentials")
     except Exception as e:
+        logger.error("Error during login for email: %s, Error: %s", user_request.email, str(e))
         raise HTTPException(status_code=500, detail=str(e))
--- ponderada_4/image_service/main.py
+++ ponderada_4/image_service/main.py
@@ -1,9 +1,13 @@
 import base64
 import io
+import logging
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from PIL import Image
 from rembg import remove
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -12,25 +16,30 @@ class ImageRequest(BaseModel):
 
 def remove_background_from_base64(base64_image: str) -> str:
     try:
+        logger.info("Starting background removal process")
         image_data = base64.b64decode(base64_image)
         image = Image.open(io.BytesIO(image_data))
 
+        logger.info("Image successfully decoded and opened")
         result_image = remove(image)
+        logger.info("Background successfully removed")
 
         buffered = io.BytesIO()
         result_image.save(buffered, format="PNG")
         base64_output_image = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        logger.info("Resulting image successfully encoded to base64")
 
         return base64_output_image
     except Exception as e:
+        logger.error("Exception during background removal: %s", str(e))
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/remove-bg")
 async def remove_bg(image_request: ImageRequest):
+    logger.info("Remove background request received")
     try:
         base64_result = remove_background_from_base64(image_request.base64_image)
         return {"base64_image": base64_result}
-    except ValueError as e:
-        return HTTPException(status_code=400, detail=str(e))
     except Exception as e:
+        logger.error("Error on image processing: %s", str(e))
         return HTTPException(status_code=500, detail=str(e))
